7f7b6a320084bb2087c579fca46650b252f66984
PHAM Quoc Anh <qpham@amaris.com>
2017-05-20 10:34:58 +0700

    Add project files.

---
+++ Arigatou.DTO/ModelContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Repository.Pattern.Ef6;
+
+namespace Arigatou.DTO
+{
+    public class ModelContext : DataContext
+    {
+        static ModelContext()
+        {
+            Database.SetInitializer<ModelContext>(null);
+        }
+        public ModelContext() : base("name=ArigatouContext")
+        {
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : Entity
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}
---
+++ Arigatou.DTO/Models/Base/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Repository.Pattern.Infrastructure;
+
+namespace Arigatou.DTO.Models.Base
+{
+    public abstract class Entity : IObjectState
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; protected set; }
+        public DateTime CreatedDate { get; set; }
+        public long CreatedById { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public long UpdatedById { get; set; }
+
+        [NotMapped]
+        public ObjectState ObjectState { get; set; }
+    }
+}
